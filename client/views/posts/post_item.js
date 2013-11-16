0a6858dd77941e073a460d6089e8da89219f0953
ilteris <ilteris@gmail.com>
2013-11-16 15:55:14 -0500

    add controller and return template helpers

---
+++ client/views/posts/post_item.js
@@ -0,0 +1,7 @@
+Template.postItem.helpers({ 
+	domain: function() {
+	var a = document.createElement('a'); a.href = this.url;
+	return a.hostname;
+	//return an anonymous function for domain helper that returns the url form post object
+	}  
+});
