0ba6aee015bb1c957ed1af2c3fb722c1e2ab1ab4
ilteris kaplan <ilteris@gmail.com>
2013-12-05 03:25:05 +0000

    define a router for new page

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -1,7 +1,9 @@
 Meteor.Router.add({
     '/': 'postsList',
+
     '/posts/:_id': {
         to: 'postPage',
         and: function(id) { Session.set('currentPostId', id); }
-}
+},
+	'/submit': 'postSubmit'
 });
