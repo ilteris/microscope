0edba225136d8c5a2f8b8976c2193bdac456c438
ilteris kaplan <ilteris@gmail.com>
2013-12-06 05:39:06 +0000

    show a loading screen while waiting to login

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -12,6 +12,8 @@ Meteor.Router.filters({
 	'requireLogin': function(page) {
 		if(Meteor.user())
 			return page;
+		else if (Meteor.loggingIn())
+			return 'loading';
 		else
 			return 'accesDenied';
 	}
