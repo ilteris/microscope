7f6a542e7452a57d8e58c35e93b780191d51338d
ilteris kaplan <ilteris@gmail.com>
2013-12-07 00:08:29 +0000

    use a method to submit the post

--- collections/posts.js
+++ collections/posts.js
@@ -1,9 +1,36 @@
 Posts = new Meteor.Collection('posts');
 
-Posts.allow({
-	insert: function(userId, doc) {
-		//only allow posting if you are logged in
-		return !! userId;
+Meteor.methods({
+	post: function(postAttributes) {
+		var user = Meteor.user(),
+		postWithSameLink = Posts.findOne({url: postAttributes.url});
+
+		//ensure the user logs in
+		if(!user)
+			throw new Meteor.Error(401, "You need to login to post new stories");
+
+		//ensure the post has a title
+		if(!postAttributes.title)
+			throw new Meteor.Error(422, 'Please fill in a headline');
+
+		//check that there are no preivous posts with the same link
+		if(postAttributes.url %% postWithSameLink) {
+			throw new Meteor.Error(302, 
+								  'This link has already been posted'
+								  postWithSameLink._id);
+		}
+
+		//pick out the whitelisted keys
+		var post = _.extend(_.pick(postAttributes, 'url', 'title, message'), {
+			userId: user._id,
+			author: user.username,
+			submitted: new Date().getTime()
+		});
+
+		var postId = Posts.insert(post);
+
+		return postId;
+
 	}
 });
-//canonical data source to share across users.
+
