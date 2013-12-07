d227047c87d163af02aa225d27c59a14b30663b6
ilteris kaplan <ilteris@gmail.com>
2013-12-07 22:39:54 +0000

    fix some typos

--- collections/posts.js
+++ collections/posts.js
@@ -14,14 +14,14 @@ Meteor.methods({
 			throw new Meteor.Error(422, 'Please fill in a headline');
 
 		//check that there are no preivous posts with the same link
-		if(postAttributes.url %% postWithSameLink) {
+		if(postAttributes.url &&  postWithSameLink) {
 			throw new Meteor.Error(302, 
-								  'This link has already been posted'
+								  'This link has already been posted',
 								  postWithSameLink._id);
 		}
 
 		//pick out the whitelisted keys
-		var post = _.extend(_.pick(postAttributes, 'url', 'title, message'), {
+		var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), {
 			userId: user._id,
 			author: user.username,
 			submitted: new Date().getTime()
